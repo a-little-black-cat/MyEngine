8f0affea04f9833dc35e6be6e32f1b7a9a5e5a0e
a-little-black-cat <berenis.tekin@gmail.com>
2025-12-29 19:40:56 -0500

    Base Manager Class and Log Manager skeleton formed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main{
     public static void main(String[] args) {
+        LogManager LM  = LogManager.getInstance();
+        LM.writeLog("Hello World. Testing LM.");
 
 
     }
